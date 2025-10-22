aa9697f44c9fb000c8c303f2f9b55efe20240877
HaDaoo <hadao1234567891011@gmail.com>
2025-10-22 20:23:56 +0700

    Update mclauncherx.py

--- mclauncherx.py
+++ mclauncherx.py
@@ -17,12 +17,23 @@ def down_java(link, java_ver):
   
   with zipfile.ZipFile(down_java_path, "r") as zip_file:
     zip_file.extractall(save_java_path)
-  
+
   os.remove(down_java_path)
 
 def down_lwjgl(type): # type = 1 -> v2.9.3; type = 2 -> v3.3.6
   if type == 1:
-    save_java_path = r"C:\Users\Admin\
+    save_lwjgl_path = r"C:\Users\Admin\AppData\Roaming\.minecraft\launcher\lwjgl\2.9.3" + "\\"
+    down_lwjgl_path = save_lwjgl_path + "lwjgl.zip"
+    urlib.request.urlretrieve("https://itzhadao.github.io/mclauncherx/lwjgl-2.9.3.zip", down_lwjgl_path)
+    
+    with zipfile.ZipFile(down_lwjgl_path, "r") as zipf:
+      zipf.extractall(save_lwjgl_path)
+    
+    os.remove(down_lwjgl_path)
+  elif type == 2:
+    save_lwjgl_path = r"C:\Users\Admin\AppData\Roaming\.minecraft\launcher\lwjgl\3.3.6" + "\\"
+    down_lwjgl_path = save_lwjgl_path + "lwjgl.zip"
+    urlib.request.urlretrieve()
 
 if os.path.isfile(launcher_file_path):
     with open(launcher_file_path, "r") as f:
