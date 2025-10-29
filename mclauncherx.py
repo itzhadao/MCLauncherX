92f0fc3f066a58cb78401f1d02371ad3fe7dc28f
HaDaoo <hadao1234567891011@gmail.com>
2025-10-30 00:10:00 +0700

    Add support for AIX PPC64 platform in JDK downloads

--- mclauncherx.py
+++ mclauncherx.py
@@ -142,6 +142,13 @@ def download_libraries():
     jdk_vers = 011
     print("DEBUG: Downloaded java versions")
     download_lwjgl()
+  elif platform_inf == "aix-ppc64":
+    down_jdk_tar("https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jdk_ppc64_aix_hotspot_8u462b08.tar.gz", 8)
+    down_jdk_tar("https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16%2B8/OpenJDK17U-jdk_ppc64_aix_hotspot_17.0.16_8.tar.gz", 17)
+    down_jdk_tar("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8%2B9/OpenJDK21U-jdk_ppc64_aix_hotspot_21.0.8_9.tar.gz", 21)
+    jdk_vers = 111
+    print("DEBUG: Downloaded java versions")
+    download_lwjgl()
 
 if __name__ == "__main__":
   clear()
